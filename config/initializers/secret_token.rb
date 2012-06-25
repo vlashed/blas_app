852a8290531337d0fceffa217ad997093aa1d6f6
branz <janetteabelgas@yahoo.com.ph>
2012-06-25 10:06:16 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlasApp::Application.config.secret_token = 'aa12f5d39c5a34c014ca8800cac52d544f87ce81552039e4529f4854f466e51b3d0d8fd3069a86f17d7c46941cc7ba06df0ec6c908b997c35fc6e677267d40cb'
